049086f8989502ec6f2833f98651371254233edd
Juan-ae <juan.avarca.0301@gmail.com>
2025-03-25 14:40:08 -0600

    fixed the login bug/issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,42 @@
 import { Box, Button } from "@mui/material";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router";
 
 export const Header = () => {
-  const { user } = useContext(AuthContext)!;
+  const { user, logout } = useContext(AuthContext)!;
+  const navigate = useNavigate();
 
-  const logoutHandler = () => {};
+  const logoutHandler = async () => {
+    if (user) {
+      try {
+        await logout();
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Box
       sx={{
         background: "grey",
         display: "flex",
-        justifyContent:"space-around",
+        justifyContent: "space-around",
         height: 50,
       }}
     >
-      <Box sx={{ mx: 1, display: "flex",
-        alignItems:"center",}}>Loan Management System</Box>
+      <Box sx={{ mx: 1, display: "flex", alignItems: "center" }}>
+        Loan Management System
+      </Box>
       {user ? (
-        <Box sx={{ mx: 1, display: "flex",
-          alignItems:"center"}}>
+        <Box sx={{ mx: 1, display: "flex", alignItems: "center" }}>
           Welcome {user?.firstName + " " + user?.lastName}
         </Box>
       ) : null}
       {user ? (
-        <Button variant="text" sx={{ mx: 1}} onClick={logoutHandler}>
+        <Button variant="text" sx={{ mx: 1 }} onClick={logoutHandler}>
           Logout
         </Button>
       ) : null}
