c4e65ec74fd41da12002227110a6ef8b44cff4b5
Juan-ae <juan.avarca.0301@gmail.com>
2025-03-28 00:44:44 -0600

    made the updateLoan feature

--- src/components/LoansCustomer.tsx
+++ src/components/LoansCustomer.tsx
@@ -1,17 +1,132 @@
-import { Box } from '@mui/material'
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { getAllUserLoans } from "../services/LoanService";
+import { Loan } from "../models/Loan";
+import { UserLoanDialog } from "./UserLoanDialog";
+import AuthContext from "../context/AuthContext";
 
 export const LoansCustomer = () => {
+  const [loans, setLoans] = useState<Loan[]>([]);
+  const [selectedLoan, setSelectedLoan] = useState<Loan | null>(null);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const { user } = useContext(AuthContext)!;
+
+  const handleRowClick = (loan: Loan) => {
+    setSelectedLoan(loan);
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setSelectedLoan(null);
+    if(user){
+      fetchAllUserLoans(user.id);
+    }
+  };
+
+  const fetchAllUserLoans = async (userId: number) => {
+    try {
+      const response = await getAllUserLoans(userId);
+      const data = await response.json();
+      if (Array.isArray(data)) {
+        setLoans(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user?.id !== undefined) {
+      fetchAllUserLoans(user.id);
+    }
+  }, []);
+
   return (
-    <Box sx={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100%",
-      width: "100%",
-      border: "1px solid red",
-    }}>
-      Loans-Customer
-    </Box>
-  )
-}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch", // Ensures children take full width
+          justifyContent: "flex-start",
+          height: "calc(100vh - 53px)", // Adjust to available height (subtract for header if needed)
+          width: "100%",
+          padding: 2,
+          overflow: "hidden",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            mb: 2,
+            borderBottom: "2px solid #D0D0D5",
+            pb: 1,
+          }}
+        >
+          My loans
+        </Typography>
+
+        <TableContainer
+          component={Paper}
+          sx={{
+            maxHeight: "80%"
+          }}
+        >
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ bgcolor: "#F0F0F1" }}>Loan #</TableCell>
+                <TableCell sx={{ bgcolor: "#F0F0F1" }}>Amount</TableCell>
+                <TableCell sx={{ bgcolor: "#F0F0F1" }}>Term</TableCell>
+                <TableCell sx={{ bgcolor: "#F0F0F1" }}>Type</TableCell>
+                <TableCell sx={{ bgcolor: "#F0F0F1" }}>Status</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {loans.map((loan, i) => (
+                <TableRow key={i} onClick={() => handleRowClick(loan)}>
+                  <TableCell>{loan.id}</TableCell>
+                  <TableCell>{loan.amount}</TableCell>
+                  <TableCell>{loan.term}</TableCell>
+                  <TableCell>{loan.type}</TableCell>
+                  <TableCell>{loan.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box sx={{ display: "flex", justifyContent: "left", mr: "auto" }}>
+          <Button
+            variant="contained"
+            sx={{
+              mt: 2,
+              width: "100%",
+              background: "#D0D0D5",
+              color: "black",
+            }}
+          >
+            Apply for a Loan
+          </Button>
+        </Box>
+      </Box>
+      <UserLoanDialog
+        open={openDialog}
+        handleClose={handleCloseDialog}
+        loan={selectedLoan}
+      />
+    </>
+  );
+};
--- src/services/LoanService.ts
+++ src/services/LoanService.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Loan } from "../models/Loan";
+
 const API_BASE_URL = "http://localhost:8080";
 
 export const getAllLoans = async () =>
@@ -14,6 +17,14 @@ export const getAllUserLoans = async (userId: number) =>
     credentials: "include",
   });
 
+export const updateLoan = async (loanId: number, updatedLoan: any) =>
+  fetch(`${API_BASE_URL}/api/loans/${loanId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(updatedLoan),
+    credentials: "include",
+  });
+
 export const approveLoan = async (loanId: number) =>
   fetch(`${API_BASE_URL}/api/loans/${loanId}/approve`, {
     method: "PATCH",
@@ -24,6 +35,13 @@ export const approveLoan = async (loanId: number) =>
 export const rejectLoan = async (loanId: number) =>
   fetch(`${API_BASE_URL}/api/loans/${loanId}/reject`, {
     method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
+
+export const getTypes = async () =>
+  fetch(`${API_BASE_URL}/api/loan-types`, {
+    method: "GET",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
   });
